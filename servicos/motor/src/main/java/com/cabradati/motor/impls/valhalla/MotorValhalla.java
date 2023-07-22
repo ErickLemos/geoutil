3e3cc42a29b1c4e422511dcf93c80a5e830cf7d8
Erick B. Lemos <ericklemoscosta@gmail.com>
2023-07-22 15:55:17 -0300

    ajustes basicos

---
+++ servicos/motor/src/main/java/com/cabradati/motor/impls/valhalla/MotorValhalla.java
@@ -0,0 +1,8 @@
+package com.cabradati.motor.impls.valhalla;
+
+import com.cabradati.motor.Motor;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class MotorValhalla implements Motor {
+}
